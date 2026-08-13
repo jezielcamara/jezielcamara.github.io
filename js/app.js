8e28a4ef10982c3266e008cb1fefda304a854baa
Jeziel Camara <camarajeziel@gmail.com>
2026-08-14 04:54:38 +0800

    Refresh Sola module cache keys

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@
 ========================================================= */
 
 import "./projects/north/project.js";
-import "./projects/sola/project.js";
+import "./projects/sola/project.js?v=20260814-2";
 
 
 /* =========================================================
